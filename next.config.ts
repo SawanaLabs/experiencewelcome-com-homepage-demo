07eb312e87eee1ed054429324ef94e1bba0a59cd
Sawana Huang <130306226+waitlistSawana@users.noreply.github.com>
2026-07-01 22:12:04 +0800

    chore: allow 127 dev origin

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["127.0.0.1"],
   reactCompiler: true,
 };
 
